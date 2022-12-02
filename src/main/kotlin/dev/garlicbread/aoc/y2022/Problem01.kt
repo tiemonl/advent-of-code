158ed2f889b3aefea603773d6e91b525adfebe09
tiemonl <tiemon.liam@gmail.com>
2022-12-02 18:35:59 -0500

    add day 1 and 2 2022

---
+++ src/main/kotlin/dev/garlicbread/aoc/y2022/Problem01.kt
@@ -0,0 +1,23 @@
+package dev.garlicbread.aoc.y2022
+
+import dev.garlicbread.aoc.Puzzle
+import dev.garlicbread.aoc.solve
+
+fun main() = solve(
+    benchmark = true
+) { Problem01() }
+
+class Problem01 : Puzzle<Int, Int>(
+    year = 2022,
+    day = 1
+) {
+    override val input = rawInput.joinToString("\n").split("\n\n").map { buildList { addAll(it.split("\n").map { it.toInt() }) } }
+
+    override fun solvePartOne(): Int {
+        return input.maxOf { it.sum() }
+    }
+
+    override fun solvePartTwo(): Int {
+        return input.groupBy { it.sum() }.map { it.key }.toList().sortedDescending().take(3).sum()
+    }
+}
