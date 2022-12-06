236006f8310640f97d8984d34ebfa6ff22dd9d42
tiemonl <tiemon.liam@gmail.com>
2022-12-06 00:35:20 -0500

    add day 6 2022

--- src/main/kotlin/dev/garlicbread/aoc/utils/String.kt
+++ src/main/kotlin/dev/garlicbread/aoc/utils/String.kt
@@ -7,3 +7,5 @@ fun String.md5(): String {
     val md = MessageDigest.getInstance("MD5")
     return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
 }
+
+fun String.allUnique(): Boolean = all(hashSetOf<Char>()::add)
