af8feff5bb9f8a40a37bcaa903fa0779f9956c6a
tiemonl <tiemon.liam@gmail.com>
2023-07-14 15:43:46 -0400

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,30 +5,44 @@ apply {
 }
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.5.31"
     application
 }
 
 group = "dev.garlicbread"
 version = "1.0"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    // Tests
     testImplementation("com.google.truth:truth:1.1.3")
-    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+    testImplementation(platform("org.junit:junit-bom:5.9.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testImplementation("org.junit.platform:junit-platform-commons:1.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-common:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(17)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "17"
 }
