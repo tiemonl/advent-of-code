a94d5e9e068401caff3a7c1316209a4240771645
tiemonl <tiemon.liam@gmail.com>
2023-11-21 20:19:28 -0500

    add 2015 day 13

--- years/2015/src/main/kotlin/dev/garlicbread/aoc/y2015/Problem13.kt
+++ years/2015/src/main/kotlin/dev/garlicbread/aoc/y2015/Problem13.kt
@@ -2,6 +2,7 @@ package dev.garlicbread.aoc.y2015
 
 import dev.garlicbread.aoc.core.Puzzle
 import dev.garlicbread.aoc.core.solve
+import dev.garlicbread.aoc.utils.permutations
 
 fun main() = solve(
     benchmark = false,
@@ -11,25 +12,29 @@ class Problem13 : Puzzle<Int, Int>(
     year = 2015,
     day = 13,
 ) {
-    // Alice would gain 54 happiness units by sitting next to Bob.
     override val input = rawInput.map { dinnerTable ->
         "\\b([A-Z].*?)\\b.*(gain|lose)\\b.*\\b([0-9]+).*?\\b([A-Z].*?)\\b.*".toRegex()
-            .matchEntire(dinnerTable)?.destructured?.let { (person, gainLose, happiness, neighbor) ->
-            DinnerTable(
-                person = person,
-                neighbor = neighbor,
-                happiness = happiness.toInt().let { if (gainLose == "lose") it.times(-1) else it },
-            )
-        }
+            .matchEntire(dinnerTable)!!.destructured.let { (person, gainLose, happiness, neighbor) ->
+                DinnerTable(
+                    person = person,
+                    neighbor = neighbor,
+                    happiness = happiness.toInt().let { if (gainLose == "lose") it.times(-1) else it },
+                )
+            }
     }
 
-    override fun solvePartOne(): Int {
-        return 0
-    }
+    private val seating = input.map { it.person }.distinct().permutations()
 
-    override fun solvePartTwo(): Int {
-        return 0
-    }
+    override fun solvePartOne() = seating.map { it + it.first() }.maxHappiness()
+
+    override fun solvePartTwo() = seating.maxHappiness()
 
     data class DinnerTable(val person: String, val neighbor: String, val happiness: Int)
+
+    private fun List<List<String>>.maxHappiness() = this.maxOf { seats ->
+        seats.zipWithNext().sumOf { (person1, person2) ->
+            input.first { it.person == person1 && it.neighbor == person2 }.happiness +
+                    input.first { it.person == person2 && it.neighbor == person1 }.happiness
+        }
+    }
 }
