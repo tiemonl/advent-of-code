33fb4a8b73fdfdbac87dbced2db59c327009a62a
tiemonl <tiemon.liam@gmail.com>
2023-12-02 09:38:41 -0500

    add Christmas color terminal output

--- modules/core/src/main/kotlin/dev/garlicbread/aoc/core/Solver.kt
+++ modules/core/src/main/kotlin/dev/garlicbread/aoc/core/Solver.kt
@@ -29,9 +29,9 @@ fun <T : Any, R : Any> solve(benchmark: Boolean = false, block: () -> Puzzle<T, 
 
 fun <T : Any, R : Any> printResult(block: () -> Puzzle<T, R>) {
     val puzzle = block()
-    val partOneResult = puzzle.solvePartOne().bold("36")
-    val partTwoResult = puzzle.solvePartTwo().bold("36")
-    println("Year ${puzzle.year}, Day ${puzzle.day}".bold())
+    val partOneResult = puzzle.solvePartOne().bold(CYAN)
+    val partTwoResult = puzzle.solvePartTwo().bold(CYAN)
+    println("${puzzle.year} - Day ${puzzle.day}\n${puzzle.name}".christmas())
     println("Part 1: $partOneResult")
     println("Part 2: $partTwoResult")
 }
@@ -59,11 +59,11 @@ private fun benchmark(block: () -> Puzzle<*, *>) {
 }
 
 private fun printBenchmark(times: Int, initTime: Float, partOneTime: Float, partTwoTime: Float) {
-    println("Initialization took " + "%.3fms".format(initTime).bold("33"))
+    println("Initialization took " + "%.3fms".format(initTime).bold(YELLOW))
     printPartBenchmark(1, partOneTime, initTime)
     printPartBenchmark(2, partTwoTime, initTime)
     println(
-        "Total: " + "%.3fms".format(initTime + partOneTime + partTwoTime).bold("33") + " (avg. of " + times.toString()
+        "Total: " + "%.3fms".format(initTime + partOneTime + partTwoTime).bold(YELLOW) + " (avg. of " + times.toString()
             .bold() + " runs)",
     )
 }
@@ -71,16 +71,30 @@ private fun printBenchmark(times: Int, initTime: Float, partOneTime: Float, part
 private fun printPartBenchmark(part: Int, time: Float, initTime: Float) {
     println(
         "Part $part took " +
-            "%.3fms".format(time).bold("33") +
+                "%.3fms".format(time).bold(YELLOW) +
             ", including init: " +
-            "%.3fms".format((initTime + time)).bold("33"),
+                "%.3fms".format((initTime + time)).bold(YELLOW),
     )
 }
 
 private fun Long.toMillis() = this / 1000000f
 
-private fun Any.bold(color: String = ""): String = style("$color;1")
+private fun Any.bold(color: String = "") = style("$color;1")
 
-private fun Any.style(color: String): String {
-    return "\u001B[${color}m$this\u001B[0m"
+private fun Any.style(color: String) = "\u001B[${color}m$this\u001B[0m"
+
+private fun String.christmas() = buildString {
+    var green = true
+    this@christmas.toCharArray().forEach {
+        val color = if (green) GREEN else RED
+        if (!it.isWhitespace()) {
+            append(it.bold(color))
+            green = !green
+        } else append(it)
+    }
 }
+
+private const val RED = "31"
+private const val GREEN = "32"
+private const val YELLOW = "33"
+private const val CYAN = "36"
