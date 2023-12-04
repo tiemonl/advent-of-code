cd00cd255a804ec29ea2c12d9e1ffbf422f22324
tiemonl <tiemon.liam@gmail.com>
2023-12-04 17:40:59 -0500

    add 2023 day 2

---
+++ years/2023/src/test/kotlin/dev/garlicbread/aoc/y2023/Problem02Test.kt
@@ -0,0 +1,56 @@
+package dev.garlicbread.aoc.y2023
+
+import com.google.common.truth.Truth.assertThat
+import dev.garlicbread.aoc.core.Solver
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("2023 Day 2")
+internal class Problem02Test {
+
+    @BeforeEach
+    fun setUp() {
+        Solver.suffix = ""
+        Solver.inputProvider = Solver.defaultInputProvider
+    }
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+        @Test
+        @DisplayName("Example Input 1")
+        fun `Matches example 1`() {
+            Solver.suffix = "_1"
+            val answer = Problem02().solvePartOne()
+            assertThat(answer).isEqualTo(8)
+        }
+
+        @Test
+        @DisplayName("Actual Input")
+        fun `Matches actual`() {
+            val answer = Problem02().solvePartOne()
+            assertThat(answer).isEqualTo(2265)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        @DisplayName("Example Input 1")
+        fun `Matches example 1`() {
+            Solver.suffix = "_1"
+            val answer = Problem02().solvePartTwo()
+            assertThat(answer).isEqualTo(2286)
+        }
+
+        @Test
+        @DisplayName("Actual Input")
+        fun `Matches actual`() {
+            val answer = Problem02().solvePartTwo()
+            assertThat(answer).isEqualTo(64097)
+        }
+    }
+}
