f9397e945e522da5255eed539248f7acf205c7f1
tiemonl <tiemon.liam@gmail.com>
2023-12-08 13:44:06 -0500

    day 8 2023

---
+++ years/2023/src/test/kotlin/dev/garlicbread/aoc/y2023/Problem08Test.kt
@@ -0,0 +1,80 @@
+package dev.garlicbread.aoc.y2023
+
+import com.google.common.truth.Truth.assertThat
+import dev.garlicbread.aoc.core.Solver
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("2023 Day 8")
+internal class Problem08Test {
+
+    @BeforeEach
+    fun setUp() {
+        Solver.suffix = ""
+        Solver.inputProvider = Solver.defaultInputProvider
+    }
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+        @Test
+        @DisplayName("Example Input 1")
+        fun `Matches example 1`() {
+            Solver.suffix = "_1"
+            val answer = Problem08().solvePartOne()
+            assertThat(answer).isEqualTo(2)
+        }
+
+        @Test
+        @DisplayName("Example Input 2")
+        fun `Matches example 2`() {
+            Solver.suffix = "_2"
+            val answer = Problem08().solvePartOne()
+            assertThat(answer).isEqualTo(6)
+        }
+
+        @Test
+        @DisplayName("Actual Input")
+        fun `Matches actual`() {
+            val answer = Problem08().solvePartOne()
+            assertThat(answer).isEqualTo(12737)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        @DisplayName("Example Input 1")
+        fun `Matches example 1`() {
+            Solver.suffix = "_1"
+            val answer = Problem08().solvePartTwo()
+            assertThat(answer).isEqualTo(2)
+        }
+
+        @Test
+        @DisplayName("Example Input 2")
+        fun `Matches example 2`() {
+            Solver.suffix = "_2"
+            val answer = Problem08().solvePartTwo()
+            assertThat(answer).isEqualTo(6)
+        }
+
+        @Test
+        @DisplayName("Example Input 3")
+        fun `Matches example 3`() {
+            Solver.suffix = "_3"
+            val answer = Problem08().solvePartTwo()
+            assertThat(answer).isEqualTo(6)
+        }
+
+        @Test
+        @DisplayName("Actual Input")
+        fun `Matches actual`() {
+            val answer = Problem08().solvePartTwo()
+            assertThat(answer).isEqualTo(9064949303801)
+        }
+    }
+}
